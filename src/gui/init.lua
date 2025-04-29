27b9d3524613bb44147aaccd8196fc03dbe0eca7
FlorzXD <108660091+FlorzXD@users.noreply.github.com>
2025-04-29 17:44:36 +1000

    Update init.lua

--- src/gui/init.lua
+++ src/gui/init.lua
@@ -1,1 +1,41 @@
+--// gui/init.lua
+
+local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source.lua"))()
+local Window = Rayfield:CreateWindow({
+    Name = "FLOORZHUB | Blox Fruits",
+    LoadingTitle = "FloorzHUB",
+    LoadingSubtitle = "by FlorzXD",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = "FloorzHUBConfig", -- Saves settings
+       FileName = "FloorzHUB"
+    },
+    Discord = {
+       Enabled = false
+    },
+    KeySystem = false -- Will be enabled later
+})
+
+-- Notifications
+Rayfield:Notify({
+    Title = "FloorzHUB Loaded",
+    Content = "Welcome back, legend.",
+    Duration = 5,
+    Image = nil
+})
+
+-- Tabs
+local tabs = {
+    Autofarm = Window:CreateTab("🧪 Autofarm"),
+    Fruits = Window:CreateTab("🍎 Devil Fruits"),
+    Teleport = Window:CreateTab("🌍 Teleport"),
+    Shop = Window:CreateTab("🛒 Shop"),
+    Misc = Window:CreateTab("🎮 Misc"),
+    Settings = Window:CreateTab("⚙️ Settings"),
+}
+
+return {
+    Rayfield = Rayfield,
+    Tabs = tabs
+}
 
