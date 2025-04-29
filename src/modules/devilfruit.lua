a5af4418cf87fbfc193f1ce51db258ffe22e7a01
FlorzXD <108660091+FlorzXD@users.noreply.github.com>
2025-04-29 18:26:55 +1000

    Update devilfruit.lua

--- src/modules/devilfruit.lua
+++ src/modules/devilfruit.lua
@@ -1,1 +1,60 @@
+-- Devil Fruit Module
+-- Handles all devil fruit related functionality
 
+local DevilFruit = {}
+DevilFruit.__index = DevilFruit
+
+function DevilFruit.new(FloorzHUB)
+    local self = setmetatable({}, DevilFruit)
+    self.FloorzHUB = FloorzHUB
+    return self
+end
+
+function DevilFruit:Init()
+    -- Initialize devil fruit settings
+    self.Config = self.FloorzHUB.Config.DevilFruit or {
+        AutoSpin = false,
+        AutoStore = true,
+        AutoEat = false,
+        FruitNotifier = true,
+        AutoGrab = true,
+        Blacklist = {}
+    }
+    
+    -- Save reference to module
+    self.FloorzHUB.Modules.DevilFruit = self
+    
+    -- Start main loops
+    task.spawn(function()
+        -- Auto spin every 2 hours
+        while task.wait(2 * 60 * 60) do
+            if self.Config.AutoSpin then
+                self:SpinFruit()
+            end
+        end
+    end)
+    
+    task.spawn(function()
+        -- Check for fruits every 10 seconds
+        while task.wait(10) do
+            if self.Config.AutoGrab or self.Config.FruitNotifier then
+                self:CheckForFruits()
+            end
+        end
+    end)
+end
+
+function DevilFruit:SpinFruit()
+    -- Implement fruit spinning logic
+    -- Would use remotes to buy random fruit
+end
+
+function DevilFruit:CheckForFruits()
+    -- Scan workspace for spawned fruits
+    -- Notify or auto-grab based on settings
+end
+
+-- Other devil fruit methods would be implemented here
+-- (StoreFruit, EatFruit, NotifyFruit, GrabFruit, etc.)
+
+return DevilFruit
